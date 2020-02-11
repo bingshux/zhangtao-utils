09d78d09975f79658f41628cbe348fffb9ed0674
bings <328922224@qq.com>
2020-02-11 15:33:58 +0800

    增加集合判断,和流处理类的4种方法

--- src/main/java/com/zhangtao/util/StreamUtil.java
+++ src/main/java/com/zhangtao/util/StreamUtil.java
@@ -1,17 +1,40 @@
 package com.zhangtao.util;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * 
+ * @ClassName: StreamUtil 
+ * @Description: 流处理类
+ * @author: bings
+ * @date: 2020年2月11日 下午3:27:41
+ */
 public class StreamUtil {
-	
-	public static List<String> readLine(InputStream in){
+	//读取文件对象到list集合中
+	public static List<String > readFile2List(File file) throws FileNotFoundException{
+			FileInputStream fileInputStream = new FileInputStream(file);
+		return readFile2List(fileInputStream);
+	}
+	//读取文件地址，并根据编码，把内容放入list集合中
+	public static List<String> readFile2List(String filename,String charset) throws FileNotFoundException{
+		FileInputStream fileInputStream = new FileInputStream(filename);
+		return readFile2List(fileInputStream,charset);
+	}
+	//读取InputStream对象，把其内容放入集合中
+	public static List<String> readFile2List(InputStream in){
+		return readFile2List(in,"utf-8");
+	}
+	//读取InputStream对象，并根据编码 把其内容放入集合中
+	public static List<String> readFile2List(InputStream inputStream,String charset){
 		List<String> list= new ArrayList<String>();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 		String str=null;
 		try {
 			while((str=reader.readLine())!=null) {
